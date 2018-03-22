41a8c6671adf151f3e2f02985a04d5729c8ad0ce
Finn <finn22fuller@yahoo.com>
2018-03-22 11:37:28 -0500

    distace update

--- distance.py
+++ distance.py
@@ -4,7 +4,44 @@ import datetime
 
 import requests
 import json
+import os
 
+
+def getFilePath():
+        maxValue = -1
+        path_to_tub_folders = os.path.dirname(os.path.realpath(__file__)) + '/data/'
+        maxFolder = ''
+        for root, dirs, files in os.walk(path_to_tub_folders, topdown=False):
+            for name in dirs:
+                if(name.startswith('tub_')):
+                    startIndex = name.find('_')
+                    endIndex = name.find('_', startIndex +1)
+                    currentValue = int(name[startIndex + 1:endIndex])
+                    if(currentValue > maxValue):
+                        maxValue = currentValue
+                        maxFolder = name
+                        
+        path_to_json = path_to_tub_folders + maxFolder + '/'
+        maxJsonValue = -1
+        maxJsonFile = ''
+        json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
+
+        for name in json_files:
+                if(name.startswith('record_')):
+                    startIndex = name.find('_')
+                    currentValue = int(name[startIndex + 1:-5])
+                    if(currentValue > maxJsonValue):
+                        maxJsonValue = currentValue
+                        maxJsonFile = name
+
+        path_to_data = path_to_json + maxJsonFile
+        json_data = open(path_to_data).read()
+
+        data = json.loads(json_data)
+        print data["user/throttle"]
+        return data["user/throttle"]
+
+        
 headers = {'content-type': 'application/json'}
 url = 'http://codejam.zrimsek.com/api/stats'
 
@@ -37,5 +74,29 @@ while True:
         distance = round(distance, 2)
         print "Distance:",distance,"cm"
         timeString = datetime.datetime.now().isoformat()
-        data = {'distance': distance, 'speed': 30, 'time': timeString}
+        maxThrottleSpeed = getFilePath()
+        data = {'distance': distance, 'speed': maxThrottleSpeed, 'time': timeString}
         requests.post(url, data=json.dumps(data), headers=headers)
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
