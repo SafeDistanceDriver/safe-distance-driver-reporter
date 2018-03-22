481898dcdd935fa9a1248baeb5271ee905c2b583
Finn <finn22fuller@yahoo.com>
2018-03-22 16:09:41 -0500

    throttle

--- distance.py
+++ distance.py
@@ -32,58 +32,21 @@ def getRating(timeToStop):
     #print('rating= ', rating)
     return rating
 
-
 def getThrottleSpeed():
-    maxValue = -1
-    path_to_tub_folders = os.path.dirname(
-        os.path.realpath(__file__)) + '/data/'
-    maxFolder = ''
-    for root, dirs, files in os.walk(path_to_tub_folders, topdown=False):
-        for name in dirs:
-            if (name.startswith('tub_')):
-                startIndex = name.find('_')
-                endIndex = name.find('_', startIndex + 1)
-                currentValue = int(name[startIndex + 1:endIndex])
-                if (currentValue > maxValue):
-                    maxValue = currentValue
-                    maxFolder = name
-
-    path_to_json = path_to_tub_folders + maxFolder + '/'
-    maxJsonValue = -1
-    maxJsonFile = ''
-    json_files = [
-        pos_json for pos_json in os.listdir(path_to_json)
-        if pos_json.endswith('.json')
-    ]
-
-    for name in json_files:
-        if (name.startswith('record_')):
-            startIndex = name.find('_')
-            currentValue = int(name[startIndex + 1:-5])
-            if (currentValue > maxJsonValue):
-                if(maxJsonFile != ''):
-                    os.remove(path_to_json + maxJsonFile)
-                maxJsonValue = currentValue
-                maxJsonFile = name
-            else:
-                os.remove(path_to_json + name)
-        else:
-            os.remove(path_to_json + name)
-    path_to_data = path_to_json + maxJsonFile
-    data = '-10'
-    if (os.stat(path_to_data).st_size > 0):
-        json_data = open(path_to_data).read()
-        data = json.loads(json_data)
-        data = data["user/throttle"]
-
-    #print("speed=", data)
-    return data
-
-getThrottleSpeed()
+    returnValue = 0.0
+    path_to_data = os.path.dirname(
+        os.path.realpath(__file__)) + '/throttle-data.txt'
+    with open(path_to_data) as file:
+        content = file.readlines()
+    for line in content:
+        if(line.startswith('throttle ')):
+            startIndex = line.find(' ')
+            returnValue = float(line[startIndex + 1:-2])
+    open(path_to_data, 'w').close()
+    return returnValue
 
 headers = {'content-type': 'application/json'}
 url = 'http://codejam.zrimsek.com/api/stats'
-
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
