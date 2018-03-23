5df973a33006babd6cb8a2dd74eea7c0e75eebde
Kevin Novak <kevinmnovak@yahoo.com>
2018-03-23 10:16:16 -0400

    Cleaned distance.py

--- distance.py
+++ distance.py
@@ -1,10 +1,11 @@
+# Standard imports
 import datetime
 import json
 import os
 import time
-
 import requests
 
+# Third party imports
 import RPi.GPIO as GPIO
 
 # Constants
@@ -13,7 +14,19 @@ MAX_THROTTLE = 1
 MIN_SPEED = 0
 MAX_SPEED = 60
 
+# Global variables
 lastValue = 0
+
+# GPIO setup
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+TRIG = 23
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.output(TRIG, False)
+
+# API setup
+headers = {'content-type': 'application/json'}
+url = 'http://codejam.zrimsek.com/api/stats'
 
 
 def calculateSpeed(throttle):
@@ -59,43 +72,37 @@ def getThrottleSpeed():
     return returnValue
 
 
-headers = {'content-type': 'application/json'}
-url = 'http://codejam.zrimsek.com/api/stats'
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+def startDataCollection():
+    time.sleep(1)
+    while True:
+        GPIO.setup(TRIG, GPIO.OUT)
+        time.sleep(.1)
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+        GPIO.setup(TRIG, GPIO.IN)
+        while GPIO.input(TRIG) == 0:
+            pulse_start = time.time()
+        while GPIO.input(TRIG) == 1:
+            pulse_end = time.time()
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17150
+        distance = round(distance, 2)
+        timeString = datetime.datetime.now().isoformat()
+        throttleValue = '-10'
+        while throttleValue == '-10':
+            throttleValue = getThrottleSpeed()
+        speed = calculateSpeed(abs(throttleValue))
+        rating = calculateRating(calculateTimeToStop(speed, distance))
+        data = {
+            'distance': distance,
+            'speed': speed,
+            'time': timeString,
+            'rating': rating
+        }
+        print("distance: " + str(distance) + "\t\tspeed: " +
+              str(speed) + "\t\trating: " + str(rating))
+        requests.post(url, data=json.dumps(data), headers=headers)
 
-TRIG = 23
 
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.output(TRIG, False)
-time.sleep(1)
-
-while True:
-    GPIO.setup(TRIG, GPIO.OUT)
-    time.sleep(.1)
-    GPIO.output(TRIG, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, False)
-    GPIO.setup(TRIG, GPIO.IN)
-    while GPIO.input(TRIG) == 0:
-        pulse_start = time.time()
-    while GPIO.input(TRIG) == 1:
-        pulse_end = time.time()
-    pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration * 17150
-    distance = round(distance, 2)
-    timeString = datetime.datetime.now().isoformat()
-    throttleValue = '-10'
-    while throttleValue == '-10':
-        throttleValue = getThrottleSpeed()
-    speed = calculateSpeed(abs(throttleValue))
-    rating = calculateRating(calculateTimeToStop(speed, distance))
-    data = {
-        'distance': distance,
-        'speed': speed,
-        'time': timeString,
-        'rating': rating
-    }
-    print("distance: " + str(distance) + "\t\tspeed: " +
-          str(speed) + "\t\trating: " + str(rating))
-    requests.post(url, data=json.dumps(data), headers=headers)
+startDataCollection()
